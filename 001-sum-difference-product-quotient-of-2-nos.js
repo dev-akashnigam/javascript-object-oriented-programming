5c35caed98e6859f48c7d3abdb8b328a171d29cc
Akash Nigam <contact.akashnigam@gmail.com>
2025-09-18 18:10:29 +0530

    Object Oriented JavaScript solution for printing the sum, difference, product and quotient of two numbers entered by the user

---
+++ 001-sum-difference-product-quotient-of-2-nos.js
@@ -0,0 +1,38 @@
+// Read two numbers and print their sum, difference, product, and quotient.
+const prompt = require('prompt-sync')();
+
+class SumDifferenceProductQuotientOf2Nos {
+    constructor(x, y) {
+        this.firstNum = x;
+        this.secondNum = y;
+    }
+
+    getSum() {
+        return this.firstNum + this.secondNum;
+    }
+
+    getDifference() {
+        return this.firstNum - this.secondNum;
+    }
+
+    getProduct() {
+        return this.firstNum * this.secondNum;
+    }
+
+    getQuotient() {
+        return this.firstNum / this.secondNum;
+    }
+}
+
+console.log("Please enter the first number: ");
+const numX = Number(prompt());
+
+console.log("Please enter the second number: ");
+const numY = Number(prompt());
+
+const obj = new SumDifferenceProductQuotientOf2Nos(numX, numY);
+
+console.log(`Sum = ${obj.getSum()}`);
+console.log(`Difference = ${obj.getDifference()}`);
+console.log(`Product = ${obj.getProduct()}`);
+console.log(`Quotient = ${obj.getQuotient()}`);
